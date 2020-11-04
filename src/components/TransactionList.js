89de473322adc4f156cd1a9893d81b898199d3d4
Narayan Pandey <npandey722@gmail.com>
2020-11-04 16:44:28 +0530

    default transaction

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,15 +1,17 @@
 import React,{useContext} from 'react'
-import {GlobalContext} from '../context/GlobalState';
-
+import {GlobalContext} from './context/GlobalState';
+import {Transaction} from './Transaction';
 export const TransactionList = () =>{
-  const context = useContext(GlobalContext)
+  const {transactions} = useContext(GlobalContext)
     return (
         <div>
         <h3>History</h3>
         <ul className="list">
-           <li className="minus">
-            Cash <span>$0.00</span><button className="delete-btn">x</button>
-          </li> 
+          {transactions.map(transaction =>(
+            <Transaction transaction={transaction}/>
+          ))}
+
+           
         </ul>
         </div>
     )
